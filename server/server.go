600e148fa0af13b46a46022d65d43ded42f138c8
jenfonro <799170122@qq.com>
2026-03-31 05:39:15 +0800

    feat(netdisk): optimize quark and uc playback flow

--- server/server.go
+++ server/server.go
@@ -87,6 +87,7 @@ func New(cfg Config) (*Server, error) {
 
 	srv := &Server{addr: cfg.Addr, db: database, mux: mux, h: handler, stop: make(chan struct{})}
 	srv.startSelfCheck()
+	srv.startPanDailyCleanup()
 	return srv, nil
 }
 
@@ -133,3 +134,88 @@ func (s *Server) startSelfCheck() {
 		}
 	}()
 }
+
+func (s *Server) startPanDailyCleanup() {
+	if s == nil || s.db == nil || s.stop == nil {
+		return
+	}
+	loc, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		loc = time.FixedZone("Asia/Shanghai", 8*60*60)
+	}
+	type providerCleanupState struct {
+		retryAt    time.Time
+		successDay string
+	}
+	go func() {
+		states := map[string]*providerCleanupState{
+			"quark": {},
+			"uc":    {},
+		}
+		attempts := map[string]func(*db.DB, time.Time) (bool, error){
+			"quark": netdiskTryQuarkDailyCleanup,
+			"uc":    netdiskTryUCDailyCleanup,
+		}
+		for {
+			now := time.Now()
+			day := now.In(loc).Format("2006-01-02")
+			target := netdiskNextDailyCleanupTime(now, loc, 6, 0)
+			for provider, st := range states {
+				if st.successDay == day {
+					continue
+				}
+				if !st.retryAt.IsZero() && st.retryAt.After(now) && st.retryAt.Before(target) {
+					target = st.retryAt
+				}
+				if st.successDay != day && netdiskNextDailyCleanupTime(now, loc, 6, 0).Before(target) {
+					target = netdiskNextDailyCleanupTime(now, loc, 6, 0)
+				}
+				_ = provider
+			}
+			timer := time.NewTimer(time.Until(target))
+			select {
+			case <-s.stop:
+				if !timer.Stop() {
+					select {
+					case <-timer.C:
+					default:
+					}
+				}
+				return
+			case <-timer.C:
+			}
+
+			runNow := time.Now()
+			runDay := runNow.In(loc).Format("2006-01-02")
+			scheduled := netdiskNextDailyCleanupTime(runNow.Add(-time.Second), loc, 6, 0)
+			for provider, st := range states {
+				if st.successDay == runDay {
+					continue
+				}
+				shouldTry := false
+				if !scheduled.After(runNow) {
+					shouldTry = true
+				}
+				if !st.retryAt.IsZero() && !st.retryAt.After(runNow) {
+					shouldTry = true
+				}
+				if !shouldTry {
+					continue
+				}
+				done, _ := attempts[provider](s.db, runNow)
+				if done {
+					st.successDay = runDay
+					st.retryAt = time.Time{}
+				} else {
+					st.retryAt = runNow.Add(10 * time.Minute)
+				}
+			}
+		}
+	}()
+}
+
+var (
+	netdiskNextDailyCleanupTime = netdisk.NextDailyCleanupTime
+	netdiskTryQuarkDailyCleanup = netdisk.TryQuarkDailyCleanup
+	netdiskTryUCDailyCleanup    = netdisk.TryUCDailyCleanup
+)
